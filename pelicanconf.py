892d8495be33d7595653d38c73ddb01750deef0d
William Setterberg <william.s.setterberg@gmail.com>
2023-01-23 11:27:34 -0600

    code and link convenience funcs; modify blurbs

--- pelicanconf.py
+++ pelicanconf.py
@@ -2,6 +2,12 @@ AUTHOR = 'William Setterberg'
 SITENAME = 'setterberg'
 SITEURL = ''
 
+def link(txt, url):
+    return f'<a target="_blank" href="{url}">{txt}</a>'
+
+def code(txt):
+    return f'<tt>{txt}</tt>'
+
 PATH = 'content'
 
 TIMEZONE = 'America/Chicago'
@@ -22,11 +28,16 @@ PAGE_ORDER_BY = 'basename'
 # Uncomment following line if you want document-relative URLs when developing
 #RELATIVE_URLS = True
 
+umn = link("University of Minnesota", "https://cse.umn.edu/physics")
+impress = link("IMPRESS", "https://smallsat.umn.edu/impress")
+wtmm = link("WTMM", "https://en.wikipedia.org/wiki/Wavelet_transform_modulus_maxima_method")
+sunkit_spex = code(link("sunkit-spex", "https://github.com/sunpy/sunxspex/issues/83"))
+stixdcpy = code(link("stixdcpy", "https://github.com/i4Ds/stixdcpy/pull/24"))
 INDEX_BLURBS = (
-    'physics PhD student at <a target="_blank" href="https://cse.umn.edu/physics">University of Minnesota</a>',
-    'build solar X-ray instruments e.g. <a target="_blank" href="https://smallsat.umn.edu/impress">IMPRESS</a>',
-    'contribute to software e.g. <tt><a target="_blank" href="https://github.com/sunpy/sunxspex/issues/83">sunkit-spex</a>,</tt>'
-    '<tt><a target="_blank" href="https://github.com/i4Ds/stixdcpy/pull/24">stixdcpy</a></tt>',
+    f'physics PhD student at {umn}',
+    f'build solar X-ray instruments e.g. {impress}',
+    f'analyze (e.g. {wtmm}) and image solar X-ray data',
+    f'contribute to software e.g. {sunkit_spex}, {stixdcpy}',
     'mentor students',
 )
 
